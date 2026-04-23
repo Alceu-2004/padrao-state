2b7fb9fd1cb3159ec0e88966da8b6e121210c808
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-04-23 11:10:00 -0300

    feat: implementação da simulação no main

--- src/main/com/player/Main.java
+++ src/main/com/player/Main.java
@@ -1,10 +1,24 @@
 package com.player;
 
 import com.player.context.MusicPlayer;
+import com.player.model.Music;
+import com.player.state.StoppedState;
 
 public class Main {
+
     public static void main(String[] args) {
+
         MusicPlayer player = new MusicPlayer();
-        System.out.println("Player iniciado");
+
+        player.addMusic(new Music("Musica 1", "Artista A", 180));
+        player.addMusic(new Music("Musica 2", "Artista B", 200));
+
+        player.setState(new StoppedState());
+
+        player.play();
+        player.pause();
+        player.play();
+        player.next();
+        player.stop();
     }
 }
